30a4787ab5a044532bb31e0c48fc6d39e509e720
Rebecca <rebecca90469@gmail.com>
2024-01-24 05:25:30 -0500

    Added working function assertEqual for assertion pass or fail

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️✔️✔️ Assertion Passed: ${actual} === ${expected}`);
+  }
+
+  if (actual !== expected) {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
