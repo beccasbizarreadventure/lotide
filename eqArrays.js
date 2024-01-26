149fc483631e02423e8abc95fcd11f0dc441078c
Rebecca <rebecca90469@gmail.com>
2024-01-26 15:59:52 -0500

    Added eqArrays function to test if values and data type of 2 arrays were equal

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️✔️✔️ Assertion Passed: ${actual} === ${expected}`);
+  }
+
+  if (actual !== expected) {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+
+    if (typeof array1[i] !== typeof array2[i]) {
+      return false;
+    }
+
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) 
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) 
