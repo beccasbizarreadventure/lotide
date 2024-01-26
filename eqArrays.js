79532f680eaf076a4e4d4e5563d8741d14c31d58
Rebecca <rebecca90469@gmail.com>
2024-01-26 16:06:07 -0500

    Added check for array length

--- eqArrays.js
+++ eqArrays.js
@@ -11,8 +11,11 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
-
     if (typeof array1[i] !== typeof array2[i]) {
+      return false;
+    }
+
+    if (array1.length !== array2.length) {
       return false;
     }
 
@@ -25,3 +28,4 @@ const eqArrays = function(array1, array2) {
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), false);
