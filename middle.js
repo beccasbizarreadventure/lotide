b30f8597ebdda638ce8be7ff603eab1a5366f52e
Rebecca <rebecca90469@gmail.com>
2024-01-27 01:18:31 -0500

    Added code to fin the middle values of even and odd arrays

---
+++ middle.js
@@ -0,0 +1,42 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === false) {
+    console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
+  }
+  if (eqArrays(array1, array2) === true) {
+    console.log(`✔️✔️✔️ Assertion Passed: ${array1} === ${array2}`);
+  }
+};
+
+const middle = function(array) {
+ let middleArray = [];
+  if (array.length % 2 !== 0) {
+   const middle = Math.floor(array.length / 2);
+   const middleValue = array[middle];
+   middleArray.push(middleValue);
+  }
+  if (array.length % 2 === 0) {
+    const middleFirst = Math.floor((array.length - 1) / 2);
+    const middleFirstValue = array[middleFirst];
+    const middleSecond = (array.length/2);
+    const middleSecondValue = array[middleSecond];
+    middleArray.push(middleFirstValue, middleSecondValue);
+  }
+  return middleArray;
+};
+
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
