8f3ee5eba7822c8c2e7c3f030cc94c9499fe4dc0
Rebecca <rebecca90469@gmail.com>
2024-01-27 03:23:21 -0500

    Added more test cases

--- middle.js
+++ middle.js
@@ -36,7 +36,10 @@ const middle = function(array) {
   return middleArray;
 };
 
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+console.log(middle([1, 2, 3])); 
+console.log(middle([1, 2, 3, 4, 5])); 
+console.log(middle([1, 2, 3, 4])); 
+console.log(middle([1, 2, 3, 4, 5, 6])); 
+
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+assertArraysEqual(middle([1, 2, 3]), [2]); 
