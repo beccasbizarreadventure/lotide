c91e5e510e8aebfb8a9bbbf7545d4294df1420ea
Rebecca <rebecca90469@gmail.com>
2024-01-27 21:53:51 -0500

    Added comments

--- tail.js
+++ tail.js
@@ -12,8 +12,10 @@ const assertEqual = function(actual, expected) {
 const tail = function(array) {
   if (array.length > 1) {
     return array.slice(1);
+    //returns the original array minus the value at index [0] (slice starts at index [1])
   } else {
     return [];
+    //returns an empty array for arrays with 1 or less values
   }
   
 };
@@ -23,6 +25,8 @@ tail(words);
 assertEqual(words.length, 3);
 assertEqual(tail(words)[0], "Lighthouse");
 assertEqual(tail(words).length, (words.length - 1));
+//the values of the arrays are not being compared, as they cannot be compared in this way
+//instead the length of the 2 arrays or the value at a specific index are compare for equivalency
 
 const words2 = ["Yo Yo"];
 assertEqual(tail(words2).length, 0);
