990956807431237b0f939f1f505ef52a14b8ac3f
Rebecca <rebecca90469@gmail.com>
2024-01-27 21:35:50 -0500

    Added comments

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,9 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✔️✔️✔️ Assertion Passed: ${actual} === ${expected}`);
   }
-
+  // if the actual inputted value is equivalent to the expected, it will print a positive message
   if (actual !== expected) {
     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
   }
-
+// if the actual inputted value is NOT equivalent to the expected, it will print a negative message
 };
