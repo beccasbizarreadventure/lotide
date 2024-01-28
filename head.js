234edb505804c956cd28a91c7d4e8641b9164cf3
Rebecca <rebecca90469@gmail.com>
2024-01-27 21:47:13 -0500

    Added comments

--- head.js
+++ head.js
@@ -12,9 +12,12 @@ const assertEqual = function(actual, expected) {
 const head = function(array) {
   if (array.length === 0) {
     return undefined;
+    //if there are no values in the array given, undefined is returned
   }
   return array[0];
+//returns the value of the first index [0] of a given array
 };
 
 assertEqual(head([5,6,7]), 5);
+//checks that the first value returned is 5
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
