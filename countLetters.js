782249efc70b783abe53ccc35fcb815acc941b1e
Rebecca <rebecca90469@gmail.com>
2024-01-28 00:07:14 -0500

    Added starter code

---
+++ countLetters.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️✔️✔️ Assertion Passed: ${actual} === ${expected}`);
+  }
+  // if the actual inputted value is equivalent to the expected, it will print a positive message
+  if (actual !== expected) {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+// if the actual inputted value is NOT equivalent to the expected, it will print a negative message
+};
