68f53b00e7bf35fe4830a813cb943f496e450edf
Rebecca <rebecca90469@gmail.com>
2024-01-29 12:57:09 -0500

    Prototype working code to return a key for a specific value

---
+++ findKeyByValue.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️✔️✔️ Assertion Passed: ${actual} === ${expected}`);
+  }
+  // if the actual inputted value is equivalent to the expected, it will print a positive message
+  if (actual !== expected) {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+// if the actual inputted value is NOT equivalent to the expected, it will print a negative message
+};
+
+const findKeyByValue = function(givenObject, wantedValue) {
+ const entriesArray = Object.entries(givenObject);
+ //converts object to an array
+ for (let entry of entriesArray) {
+  //loops through all entries in the array
+  if (entry.includes(wantedValue)) {
+    return entry[0];
+    //returns the first element of the [key, value] paired array
+  }
+ }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
