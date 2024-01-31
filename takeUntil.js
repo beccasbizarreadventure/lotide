ee5981201410c74a5275d02ca600d805e3650aa3
Rebecca <rebecca90469@gmail.com>
2024-01-30 23:49:21 -0500

    Fixed loop to have correct variables, added eqArrays funtion

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,15 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const assertArraysEqual = function(input, expected) {
   if (eqArrays(input, expected) === false) {
     console.log(`❌❌❌ Assertion Failed: ${input} !== ${expected}`);
@@ -11,22 +23,25 @@ const assertArraysEqual = function(input, expected) {
 const takeUntil = function(array, callback) {
   const results = [];
   for (let item of array) {
-    if (!callback[item]) {
-      results.push(array[item]);
+    if (!callback(item)) {
+      results.push(item);
     }
-    if (callback[item]) {
+    //calls function callback on current item of array and pushes it to empty results array if it's not the same result as the callback parameters
+    if (callback(item)) {
       break;
+      //breaks the loops as soon as === callback(item)
     }
   }
-return results;
+  return results;
+//returns array
 };
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
 
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
