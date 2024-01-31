26f17ae91eb3e0a75f8d0f3fd84805bf64dccffb
Rebecca <rebecca90469@gmail.com>
2024-01-30 23:26:28 -0500

    I'm on the right track, just committing to test

---
+++ takeUntil.js
@@ -0,0 +1,32 @@
+const assertArraysEqual = function(input, expected) {
+  if (eqArrays(input, expected) === false) {
+    console.log(`❌❌❌ Assertion Failed: ${input} !== ${expected}`);
+  }
+  if (eqArrays(input, expected) === true) {
+    console.log(`✔️✔️✔️ Assertion Passed: ${input} === ${expected}`);
+  }
+};
+
+
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    if (array[item] !== callback[item]) {
+      results.push(array[item]);
+    }
+    if (array[item] === callback[item]) {
+      return results;
+    }
+  }
+
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
