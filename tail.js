fd76937e9cfa0e009f321a06244c14b1dd90c231
Rebecca <rebecca90469@gmail.com>
2024-02-09 17:15:34 -0500

    Removed test cases and exported tail funtion

--- tail.js
+++ tail.js
@@ -1,14 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔️✔️✔️ Assertion Passed: ${actual} === ${expected}`);
-  }
-
-  if (actual !== expected) {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-
 const tail = function(array) {
   if (array.length > 1) {
     return array.slice(1);
@@ -20,16 +9,4 @@ const tail = function(array) {
   
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-assertEqual(tail(words)[0], "Lighthouse");
-assertEqual(tail(words).length, (words.length - 1));
-//the values of the arrays are not being compared, as they cannot be compared in this way
-//instead the length of the 2 arrays or the value at a specific index are compare for equivalency
-
-const words2 = ["Yo Yo"];
-assertEqual(tail(words2).length, 0);
-
-const words3 = [];
-assertEqual(tail(words3).length, 0);
+module.exports = tail;
