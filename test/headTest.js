1e1b3805788c215180db7cf8cfb22d2d2c9df6b2
Rebecca <rebecca90469@gmail.com>
2024-02-10 03:39:02 -0500

    Altered tests to use chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,15 @@
-const assertEqual = require('../assertEqual');
 const head = require('../head');
+const assert = require('chai').assert;
 
-assertEqual(head([5,6,7]), 5);
-//checks that the first value returned is 5
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns the first value of an array such as 1 for [1, 2, 3])", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns 'Hello' when given the array '[Hello, Lighthouse, Labs]'", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+  
+});
