0ad1323544aafabace4f38d03e9e1a94d529d7fd
Rebecca <rebecca90469@gmail.com>
2024-02-09 22:42:39 -0500

    Fixed require for eqArrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(input, expected) {
   if (eqArrays(input, expected) === false) {
@@ -18,10 +8,5 @@ const assertArraysEqual = function(input, expected) {
     console.log(`✔️✔️✔️ Assertion Passed: ${input} === ${expected}`);
   }
 };
-//assertArraysEqual calls function eqArray to test the equivelency of 2 arrays (input and expected) for length and equivalent value at each index
-//logs appropriate message based on return false or true
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3", "4"]);
 
 module.exports = assertArraysEqual;
